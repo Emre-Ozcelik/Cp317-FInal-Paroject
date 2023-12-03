b9a30b5c5ae3e12ed821e56f291b02a2911e3251
Emre Ozcelik <102702926+Emre-Ozcelik@users.noreply.github.com>
2023-12-03 01:51:10 -0500

    Gui complete

--- gui.java
+++ gui.java
@@ -1,38 +1,64 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.io.*;
-import java.util.*;
+
 public class gui extends JFrame {
 
-    private JTextArea filePreviewArea1;
-    private JTextArea filePreviewArea2;
-    private String filePath1;
-    private String filePath2;
+    private JTextArea filePreviewArea1, filePreviewArea2, filePreviewArea3, filePreviewArea4;
+    private String filePath1, filePath2;
+    private JLabel filePreviewLabel1, filePreviewLabel2;
     private final String pathToErrorLogFile = "ErrorLog.txt"; // Default path for ErrorLog.txt
     private final String pathToResultsFile = "Results.txt";   // Default path for Results.txt
 
     public gui() {
         // Set up the frame
-        setTitle("File Chooser GUI with Preview");
+        setTitle("Final Marks Calculator v1.0");
         setSize(800, 600); // Size of the GUI
         setLocationRelativeTo(null); // Center the window
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        initComponents(); // Initialize and set up components
+    }
+
+    private void initComponents() {
+        // Create labels
+        filePreviewLabel1 = new JLabel("Preview of Grades Data File");
+        filePreviewLabel2 = new JLabel("Preview of Student's Names File");
+
         // Create buttons
-        JButton chooseFile1Button = new JButton("Choose Course File");
-        JButton chooseFile2Button = new JButton("Choose Name File");
-        JButton calculateButton = new JButton("Calculate");
+        JButton chooseFile1Button = new JButton("Select Grades Data File");
+        JButton chooseFile2Button = new JButton("Select Student's Names File");
+        JButton calculateButton = new JButton("Calculate Final Marks");
         JButton closeButton = new JButton("Close");
 
+        // Set button colors
+        chooseFile1Button.setBackground(Color.BLUE);
+        chooseFile1Button.setForeground(Color.WHITE);
+        chooseFile2Button.setBackground(Color.BLUE);
+        chooseFile2Button.setForeground(Color.WHITE);
+        calculateButton.setBackground(new Color(0, 128, 0)); // Dark green
+        calculateButton.setForeground(Color.WHITE);
+        closeButton.setBackground(Color.RED);
+        closeButton.setForeground(Color.WHITE);
+
+        // Ensure buttons are opaque to show background color
+        chooseFile1Button.setOpaque(true);
+        chooseFile1Button.setBorderPainted(false); // Optional, for a flat look
+        chooseFile2Button.setOpaque(true);
+        chooseFile2Button.setBorderPainted(false); // Optional, for a flat look
+        calculateButton.setOpaque(true);
+        calculateButton.setBorderPainted(false); // Optional, for a flat look
+        closeButton.setOpaque(true);
+        closeButton.setBorderPainted(false); // Optional, for a flat look
+
         // Create text areas for file previews
-        filePreviewArea1 = new JTextArea(15, 30); // Size of text areas
-        filePreviewArea2 = new JTextArea(15, 30); // Size of text areas
-        filePreviewArea1.setEditable(false);
-        filePreviewArea2.setEditable(false);
+        filePreviewArea1 = new JTextArea(15, 30);
+        filePreviewArea2 = new JTextArea(15, 30);
+        filePreviewArea3 = new JTextArea(15, 30);
+        filePreviewArea4 = new JTextArea(15, 30);
+        setEditableForTextAreas(false); // Set text areas to non-editable
 
         // Add action listeners
         chooseFile1Button.addActionListener(e -> chooseFile(1));
@@ -47,13 +73,54 @@ public class gui extends JFrame {
         buttonPanel.add(calculateButton);
         buttonPanel.add(closeButton);
 
-        JPanel previewPanel = new JPanel();
-        previewPanel.add(new JScrollPane(filePreviewArea1));
-        previewPanel.add(new JScrollPane(filePreviewArea2));
+        JPanel mainPreviewPanel = createPreviewPanel(); // Create panel with previews and labels
 
+        // Add panels to frame
         setLayout(new BorderLayout());
         add(buttonPanel, BorderLayout.NORTH);
-        add(previewPanel, BorderLayout.CENTER);
+        add(mainPreviewPanel, BorderLayout.CENTER);
+    }
+
+    private void setEditableForTextAreas(boolean editable) {
+        filePreviewArea1.setEditable(editable);
+        filePreviewArea2.setEditable(editable);
+        filePreviewArea3.setEditable(editable);
+        filePreviewArea4.setEditable(editable);
+    }
+
+    private JPanel createPreviewPanel() {
+        // Create Labels
+        JLabel resultsPreviewLabel = new JLabel("Results Preview");
+        JLabel errorLogPreviewLabel = new JLabel("Error Log Preview");
+
+        // Create Panels for previews
+        JPanel previewPanel1 = createIndividualPreviewPanel(filePreviewLabel1, filePreviewArea1);
+        JPanel previewPanel2 = createIndividualPreviewPanel(filePreviewLabel2, filePreviewArea2);
+        JPanel previewPanel3 = createIndividualPreviewPanel(resultsPreviewLabel, filePreviewArea3);
+        JPanel previewPanel4 = createIndividualPreviewPanel(errorLogPreviewLabel, filePreviewArea4);
+
+        // Main preview panel
+        JPanel mainPreviewPanel = new JPanel(new GridLayout(2, 2));
+        mainPreviewPanel.add(previewPanel1);
+        mainPreviewPanel.add(previewPanel2);
+        mainPreviewPanel.add(previewPanel3);
+        mainPreviewPanel.add(previewPanel4);
+
+        return mainPreviewPanel;
+    }
+
+
+    private JPanel createIndividualPreviewPanel(JLabel label, JTextArea textArea) {
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(label, BorderLayout.NORTH);
+
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        panel.add(scrollPane, BorderLayout.CENTER);
+
+        // Set a margin around the JScrollPane
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // 10 pixels margin on all sides
+
+        return panel;
     }
 
     private void chooseFile(int fileNumber) {
@@ -62,28 +129,40 @@ public class gui extends JFrame {
 
         if (result == JFileChooser.APPROVE_OPTION) {
             File selectedFile = fileChooser.getSelectedFile();
+            String fileName = selectedFile.getName(); // Get the name of the selected file
             try {
                 String content = new String(Files.readAllBytes(Paths.get(selectedFile.getAbsolutePath())));
                 if (fileNumber == 1) {
                     filePreviewArea1.setText(content);
                     filePath1 = selectedFile.getAbsolutePath();
+                    filePreviewLabel1.setText("Preview of " + fileName); // Update the label with the file name
                 } else {
                     filePreviewArea2.setText(content);
                     filePath2 = selectedFile.getAbsolutePath();
+                    filePreviewLabel2.setText("Preview of " + fileName); // Update the label with the file name
                 }
             } catch (Exception e) {
                 JOptionPane.showMessageDialog(this, "Error reading file: " + e.getMessage());
             }
         }
     }
-
+    
     private void runMainLogic() {
         if (filePath1 != null && filePath2 != null) {
             new Thread(() -> {
                 try {
-                    System.out.println(" anythign " + filePath1 + " \n " + filePath2);
+                    System.out.println(" anything " + filePath1 + " \n " + filePath2);
                     Main.processFiles(filePath2, filePath1, pathToErrorLogFile, pathToResultsFile);
                     JOptionPane.showMessageDialog(this, "Processing completed successfully.");
+
+                    // Display the results file
+                    String content = new String(Files.readAllBytes(Paths.get(pathToResultsFile)));
+                    filePreviewArea3.setText(content);
+
+                    // Display the error log file
+                    content = new String(Files.readAllBytes(Paths.get(pathToErrorLogFile)));
+                    filePreviewArea4.setText(content);
+
                 } catch (Exception e) {
                     JOptionPane.showMessageDialog(this, "Error during processing: " + e.getMessage());
                 }
@@ -92,6 +171,7 @@ public class gui extends JFrame {
             JOptionPane.showMessageDialog(this, "Please select both files first.");
         }
     }
+
 
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
